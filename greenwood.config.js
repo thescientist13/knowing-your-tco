22ded5a7a21d3c97ccec3979f312fa117636312b
Owen Buckley <owenbuckley13@gmail.com>
2021-08-10 12:48:25 -0400

    its working on its own woohoo

--- greenwood.config.js
+++ greenwood.config.js
@@ -1,25 +1,6 @@
 const pluginGraphQL = require('@greenwood/plugin-graphql');
 const pluginImportCss = require('@greenwood/plugin-import-css');
 const pluginThemePresentation = require('greenwood-starter-presentation');
-// const { ResourceInterface } = require('@greenwood/cli/src/lib/resource-interface');
-
-// class ThemePackPageOverrideResource extends ResourceInterface {
-//   constructor(compilation, options) {
-//     super(compilation, options);
-//     this.extensions = ['*'];
-//   }
-
-//   async shouldIntercept(url) {
-//     console.debug('ThemePackPageOverrideResource shouldIntercept', url)
-//     return Promise.resolve(url.replace(this.compilation.context.userWorkspace, '') === '/');
-//   }
-
-//   async intercept(url) {
-//     console.debug('ThemePackPageOverrideResource intercept', url)
-//     return Promise.resolve({ body: 'hello world!'});
-//   }
-// }
-
 
 module.exports = {
   title: 'Knowing Your TCO',
@@ -28,10 +9,5 @@ module.exports = {
     ...pluginImportCss(),
     ...pluginGraphQL(),
     ...pluginThemePresentation()
-    // {
-    //   type: 'resource',
-    //   name: 'my-theme-pack:resource',
-    //   provider: (compilation, options) => new ThemePackPageOverrideResource(compilation, options)
-    // }
   ]
 };
