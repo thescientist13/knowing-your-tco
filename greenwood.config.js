6309999d7c4998b6d3f436b5277b52d5682d6220
Owen Buckley <owenbuckley13@gmail.com>
2023-11-15 08:57:55 -0500

    latest presentation starter patch for bg flicker

--- greenwood.config.js
+++ greenwood.config.js
@@ -1,8 +1,10 @@
+import { greenwoodPluginImportCss } from '@greenwood/plugin-import-css';
 import { greenwoodThemeStarterPresentation } from 'greenwood-starter-presentation';
 
 export default {
   title: 'Knowing Your TCO',
   plugins: [
+    greenwoodPluginImportCss(),
     greenwoodThemeStarterPresentation()
   ]
 };
